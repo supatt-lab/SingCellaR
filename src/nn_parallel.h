08f6a735a65730a9cc6d214ae3a7cd32124031c2
Supat Thongjuea <supat.thongjuea@gmail.com>
2023-08-07 16:26:23 +0200

    Update nn_parallel.h
    Updated nn_parallel.h to fix to installation problem

--- src/nn_parallel.h
+++ src/nn_parallel.h
@@ -21,7 +21,35 @@
 
 #include "RcppAnnoy.h"
 
-#include "matrix.h"
+#if ANNOY_VERSION >= Annoy_Version(1,17,3)
+
+typedef Annoy::AnnoyIndexSingleThreadedBuildPolicy AnnoyIndexThreadedBuildPolicy;
+
+struct UwotAnnoyEuclidean {
+  using Distance = Annoy::Euclidean;
+  using S = int32_t;
+  using T = float;
+};
+
+struct UwotAnnoyCosine {
+  using Distance = Annoy::Angular;
+  using S = int32_t;
+  using T = float;
+};
+
+struct UwotAnnoyManhattan {
+  using Distance = Annoy::Manhattan;
+  using S = int32_t;
+  using T = float;
+};
+
+struct UwotAnnoyHamming {
+  using Distance = Annoy::Hamming;
+  using S = int32_t;
+  using T = uint64_t;
+};
+
+#else
 
 typedef AnnoyIndexSingleThreadedBuildPolicy AnnoyIndexThreadedBuildPolicy;
 
@@ -49,6 +77,8 @@ struct UwotAnnoyHamming {
   using T = uint64_t;
 };
 
+#endif  // of 'if ANNOY_VERSION >= Annoy_Version(1,17,3)'
+
 template <typename UwotAnnoyDistance> struct NNWorker {
   const std::string &index_name;
   const std::vector<double> &mat;
@@ -58,40 +88,48 @@ template <typename UwotAnnoyDistance> struct NNWorker {
   std::size_t search_k;
   std::vector<int> idx;
   std::vector<typename UwotAnnoyDistance::T> dists;
-
+  
+#if ANNOY_VERSION >= Annoy_Version(1,17,3)
+  Annoy::AnnoyIndex<typename UwotAnnoyDistance::S, typename UwotAnnoyDistance::T,
+                    typename UwotAnnoyDistance::Distance, Kiss64Random,
+                    AnnoyIndexThreadedBuildPolicy>
+    index;
+#else
   AnnoyIndex<typename UwotAnnoyDistance::S, typename UwotAnnoyDistance::T,
-             typename UwotAnnoyDistance::Distance, Kiss64Random, AnnoyIndexThreadedBuildPolicy>
-      index;
-
+             typename UwotAnnoyDistance::Distance, Kiss64Random,
+             AnnoyIndexThreadedBuildPolicy>
+    index;
+#endif
+  
   NNWorker(const std::string &index_name, const std::vector<double> &mat,
            std::size_t ncol, std::size_t n_neighbors, std::size_t search_k)
-      : index_name(index_name), mat(mat), nrow(mat.size() / ncol), ncol(ncol),
-        n_neighbors(n_neighbors), search_k(search_k),
-        idx(nrow * n_neighbors, -1), dists(nrow * n_neighbors), index(ncol) {
+    : index_name(index_name), mat(mat), nrow(mat.size() / ncol), ncol(ncol),
+      n_neighbors(n_neighbors), search_k(search_k),
+      idx(nrow * n_neighbors, -1), dists(nrow * n_neighbors), index(ncol) {
     index.load(index_name.c_str());
   }
   
-  ~NNWorker() { 
-    index.unload();
-  }
-
+  ~NNWorker() { index.unload(); }
+  
   void operator()(std::size_t begin, std::size_t end) {
     for (auto i = begin; i < end; i++) {
       std::vector<typename UwotAnnoyDistance::T> fv(ncol);
-      uwot::get_row(mat, nrow, ncol, i, fv);
-
+      for (std::size_t j = 0; j < ncol; j++) {
+        fv[j] = mat[i + j * nrow];
+      }
+      
       std::vector<typename UwotAnnoyDistance::S> result;
       std::vector<typename UwotAnnoyDistance::T> distances;
-
+      
       index.get_nns_by_vector(fv.data(), n_neighbors, search_k, &result,
                               &distances);
       if (result.size() != n_neighbors || distances.size() != n_neighbors) {
         break;
       }
-
+      
       for (std::size_t j = 0; j < n_neighbors; j++) {
-        uwot::set_row(dists, nrow, n_neighbors, i, distances);
-        uwot::set_row(idx, nrow, n_neighbors, i, result);
+        dists[i + j * nrow] = distances[j];
+        idx[i + j * nrow] = result[j];
       }
     }
   }
